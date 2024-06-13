2dc72dc12529feda4ed7c84fb2bc1c532709bf09
CryptogramPaul <153927676+CryptogramPaul@users.noreply.github.com>
2024-06-13 11:36:35 +0800

    compare hash

--- server.js
+++ server.js
@@ -9,9 +9,16 @@ const saltRounds = 12;
 const myPlaintextPassword = "sUperpassw0rd!";
 const someOtherPlaintextPassword = "pass123";
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash);
+  bcrypt.compare(someOtherPlaintextPassword, hash, (err, res) => {
+    console.log(res);
+  });
 });
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
